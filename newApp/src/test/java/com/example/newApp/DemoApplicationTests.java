47d19183f3954e59a58134b808c873f0d3aa0fb6
zhuje <59589720+zhuje@users.noreply.github.com>
2020-09-15 11:54:58 -0400

    add newApp

---
+++ newApp/src/test/java/com/example/newApp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
