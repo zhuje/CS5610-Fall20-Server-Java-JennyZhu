6f79cfe4f6dd280b3ddf56659374bfa15927b903
zhuje <59589720+zhuje@users.noreply.github.com>
2020-09-27 00:42:45 -0400

    update URL with my unique ID

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -4,18 +4,18 @@ function AdminUserServiceClient() {
     this.findUserById = findUserById;
     this.deleteUser = deleteUser;
     this.updateUser = updateUser;
-    this.url = 'https://wbdv-generic-server.herokuapp.com/api/jannunzi/users';
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/zhuje/users';
     var self = this;
 
     // DEFAULT method is : GET
     function findAllUsers() {
-        return fetch('https://wbdv-generic-server.herokuapp.com/api/jannunzi/users')
+        return fetch('https://wbdv-generic-server.herokuapp.com/api/zhuje/users')
             .then(response => response.json())
     }
 
     // POST
     function createUser(user) {
-        return fetch('https://wbdv-generic-server.herokuapp.com/api/jannunzi/users', {
+        return fetch('https://wbdv-generic-server.herokuapp.com/api/zhuje/users', {
             method: 'POST', // adds new data
             body: JSON.stringify(user), // convert user objects to string
             headers: {
@@ -33,7 +33,7 @@ function AdminUserServiceClient() {
 
     // PUT
     function updateUser(userId, user) {
-        return fetch(`https://wbdv-generic-server.herokuapp.com/api/jannunzi/users/${userId}`, {
+        return fetch(`https://wbdv-generic-server.herokuapp.com/api/zhuje/users/${userId}`, {
             method: 'PUT', // Edits data
             body: JSON.stringify(user),
             headers: {
